3ca840dc3f4e7f5d03a4d90690e3ec4a9042efe4
Daiki Ihara <sasurau4@gmail.com>
2019-06-04 20:44:15 +0900

    initial commit

---
+++ .fusumarc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  meta: {
+    author: '',
+    siteName: '',
+    repositoryUrl: '',
+    sns: ['twitter', 'hatena'],
+  },
+  slide: {
+    loop: false,
+    sidebar: true,
+    targetBlank: true,
+    showIndex: false,
+    isVertical: false,
+    code: {
+      languages: ['javascript', 'json'],
+      plugins: ['line-numbers'],
+      theme: 'solarizedlight',
+    },
+  },
+  extends: {
+    js: '',
+    css: 'index.css',
+  },
+};
