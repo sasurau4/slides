656bd625958d734ec96d0a55cf0e20c4345c9809
Daiki Ihara <sasurau4@gmail.com>
2019-10-09 17:36:10 +0900

    add pdf script

---
+++ scripts/pdf.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const { join } = require('path')
+const pdf = require('@fusuma/task-pdf')
+const fs = require('fs-extra')
+
+const [, , output] = process.argv
+
+if (!output) process.exit(1)
+;(async () => {
+  const base = join(process.cwd(), output)
+  const port = 3455
+
+  await pdf(join(base, 'dist'), join(base, 'slide.pdf'), port)
+})()
